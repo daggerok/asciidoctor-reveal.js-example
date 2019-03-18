4327fa63e6006891bb4c24371b70cb536ce5f5d3
Maksim Kostromin <daggerok@gmail.com>
2019-03-18 05:28:53 +0200

    GitHub pages.

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 // Load asciidoctor.js and asciidoctor-reveal.js
 var asciidoctor = require('asciidoctor.js')();
 require('asciidoctor-reveal.js').register();
+// use cross-env to setup <base href="...." />
+var baseHref = '/asciidoctor-reveal.js-example/';
+var attributes = !process.env.BASE_HREF ? {} : { revealjsdir: baseHref + 'node_modules/reveal.js' };
 // Convert the document 'README.adoc' using the reveal.js converter
 asciidoctor.convertFile('README.adoc', {
   safe: 'safe',
   backend: 'revealjs',
   to_file: './index.html',
-  sourcemap: true,
+  attributes,
+  // ...
   // see reveal.js options here: https://github.com/asciidoctor/asciidoctor-reveal.js#reveal-js-options
-  // attributes,
-  // header_footer,
-  // header_only,
-  // base_dir,
-  // backend,
-  // doctype,
-  // cursor,
-  // extension_registry,
-  // extensions,
-  // parse_header_only,
-  // catalog_assets,
-  // template_dirs,
-  // template_engine,
-  // template_engine_options,
-  // eruby,
-  // converter,
 });
